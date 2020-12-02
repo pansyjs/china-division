8ad03e093ce565d0440295f314a7ca8ef3e1ed55
wangxingkang <156148958@qq.com>
2020-12-02 19:14:59 +0800

    feat(utils): 通用方法封装

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -36,11 +36,54 @@ beforeEach(() => {
 });
 
 describe('DivisionUtil', () => {
-  it('获取省级数据', () => {
-    expect(divisionUtil.getChildrenByCode()).toEqual([{ v: '110000', n: '北京市' }]);
-  });
+  describe('getChildrenByCode', () => {
+    it('获取省级数据', () => {
+      expect(divisionUtil.getChildrenByCode()).toEqual([{ v: '110000', n: '北京市' }]);
+    });
 
-  it('获取市级数据', () => {
-    expect(divisionUtil.getChildrenByCode('110100')).toEqual([]);
-  });
+    it('获取市级数据', () => {
+      expect(divisionUtil.getChildrenByCode('110000')).toEqual([
+        { v: '110100', n: '市辖区' }
+      ]);
+    });
+
+    it('获取区域数据', () => {
+      expect(divisionUtil.getChildrenByCode('110100')).toEqual([
+        { v: '110101', n: '东城区' },
+        { v: '110102', n: '西城区' },
+        { v: '110105', n: '朝阳区' },
+        { v: '110106', n: '丰台区' },
+        { v: '110107', n: '石景山区' },
+        { v: '110108', n: '海淀区' },
+        { v: '110109', n: '门头沟区' },
+        { v: '110111', n: '房山区' },
+        { v: '110112', n: '通州区' },
+        { v: '110113', n: '顺义区' },
+        { v: '110114', n: '昌平区' },
+        { v: '110115', n: '大兴区' },
+        { v: '110116', n: '怀柔区' },
+        { v: '110117', n: '平谷区' },
+        { v: '110118', n: '密云区' },
+        { v: '110119', n: '延庆区' }
+      ]);
+    });
+  })
+
+  describe('getChildrenByCode', () => {
+    it('获取省级名称', () => {
+      expect(divisionUtil.getNameByCode('110000')).toEqual('北京市');
+    });
+
+    it('获取市级名称', () => {
+      expect(divisionUtil.getNameByCode('110100')).toEqual('市辖区');
+    });
+
+    it('获取区级名称', () => {
+      expect(divisionUtil.getNameByCode('110112')).toEqual('通州区');
+    });
+
+    it('获取不存在的省市区', () => {
+      expect(divisionUtil.getNameByCode('999999')).toEqual('');
+    });
+  })
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
 import { DivisionData } from './types';
 
-class DivisionUtil<T extends object = DivisionData> {
+class DivisionUtil {
   private divisions: DivisionData[];
-  private cache: Record<string, DivisionData[]>;
+  private cache: Record<string, any>;
 
   constructor(list: DivisionData[]) {
     this.divisions = list;
@@ -10,7 +10,14 @@ class DivisionUtil<T extends object = DivisionData> {
   }
 
   /**
-   * 获取省级别的数据集合
+   * 获取数据源
+   */
+  getSourceData = () => {
+    return this.divisions ?? [];
+  }
+
+  /**
+   * 获取省级别的数据
    */
   getProvinces = (): DivisionData[] => {
     if (!Array.isArray(this.divisions) || this.divisions.length === 0) return [];
@@ -26,38 +33,92 @@ class DivisionUtil<T extends object = DivisionData> {
     }
   };
 
-  /**
-   * 获取指定省市区编码的下一级数据。 若不指定，则获取省一级数据
-   * @param list 省市区树形数据
-   * @param code 省市区编码
-   */
-  getChildrenByCode = (code?: string) => {
-    if (!code) return this.getProvinces();
-    if (typeof code !== 'string' || code.length !== 6) return [];
-
+  _getCodes = (code: string): string[] => {
     let codes: string[] = [];
 
     for (let i = 0; i < 3; i++) {
       codes.push(code.slice(i * 2, i * 2 + 2));
     }
+    codes = codes.filter(item => +item);
 
-    codes = codes.filter((item) => +item);
+    return codes;
+  }
 
-    codes.forEach((item, index, arr) => {
-      const codeStr = ``;
-    });
+  _getProvinceData = (code: string): DivisionData => {
+    let province: DivisionData;
 
-    console.log(codes);
+    // 尝试读取缓存
+    if (this.cache[code]) {
+      province = this.cache[code];
+    } else {
+      for (let i = 0; i < this.divisions.length; i++) {
+        if (this.divisions[i].v.startsWith(code)) {
+          this.cache[code] = province = this.divisions[i];
+          break;
+        }
+      }
+    }
 
-    // 110100
-    //
+    return province;
+  }
 
-    // codes = codes.map(item => {
+  /**
+   * 获取指定省市区编码的下一级数据。若不指定，则获取省一级数据
+   * @param code 省市区编码
+   */
+  getChildrenByCode = (code?: string) => {
+    if (!code) return this.getProvinces();
+    if (typeof code !== 'string' || code.length !== 6) return [];
+    if (this.cache[code]) return this.cache[code];
 
-    // })
+    const codes: string[] = this._getCodes(code);
+    const province = this._getProvinceData(codes[0]);
 
-    return [];
+    if (!province) return [];
+
+    let children = province.c ?? [];
+
+    for (let i = 1; i < codes.length; i++) {
+      const currentCode = codes.slice(0, i).reduce((next, current) => next + current, '');
+
+      for (let item of children) {
+        if (item.v.startsWith(currentCode)) {
+          children = item.c ?? [];
+        }
+      }
+    }
+
+    const list = children.map(item => ({ v: item.v, n: item.n }));
+
+    this.cache[code] = list;
+
+    return list;
   };
+
+  /**
+   * 通过省市区编码获取省市区名称
+   * @param code
+   */
+  getNameByCode = (code: string): string => {
+    const codes: string[] = this._getCodes(code);
+    const province = this._getProvinceData(codes[0]);
+
+    if (!province) return '';
+
+    let divisionData = province;
+
+    for (let i = 1; i < codes.length; i++) {
+      const currentCode = codes.slice(0, i + 1).reduce((next, current) => next + current, '');
+
+      for (let item of (divisionData.c || [])) {
+        if (item.v.startsWith(currentCode)) {
+          divisionData = item;
+        }
+      }
+    }
+
+    return divisionData.n ?? '';
+  }
 }
 
 export default DivisionUtil;
