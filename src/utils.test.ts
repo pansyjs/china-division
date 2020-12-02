db69e4eb3714615e7c4079810347239888726d4c
wangxingkang <156148958@qq.com>
2020-12-02 23:22:12 +0800

    feat: 支持定制化输出格式

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,41 +1,53 @@
 import DivisionUtil from './utils';
+import { CascaderOption, DivisionData } from './types';
+
+const sourceData = [
+  {
+    v: '110000',
+    n: '北京市',
+    c: [
+      {
+        v: '110100',
+        n: '市辖区',
+        c: [
+          { v: '110101', n: '东城区' },
+          { v: '110102', n: '西城区' },
+          { v: '110105', n: '朝阳区' },
+          { v: '110106', n: '丰台区' },
+          { v: '110107', n: '石景山区' },
+          { v: '110108', n: '海淀区' },
+          { v: '110109', n: '门头沟区' },
+          { v: '110111', n: '房山区' },
+          { v: '110112', n: '通州区' },
+          { v: '110113', n: '顺义区' },
+          { v: '110114', n: '昌平区' },
+          { v: '110115', n: '大兴区' },
+          { v: '110116', n: '怀柔区' },
+          { v: '110117', n: '平谷区' },
+          { v: '110118', n: '密云区' },
+          { v: '110119', n: '延庆区' }
+        ]
+      }
+    ]
+  }
+]
 
 let divisionUtil: DivisionUtil;
+let divisionUtil1: DivisionUtil<DivisionData, CascaderOption>;
+
 
 beforeEach(() => {
-  divisionUtil = new DivisionUtil([
-    {
-      v: '110000',
-      n: '北京市',
-      c: [
-        {
-          v: '110100',
-          n: '市辖区',
-          c: [
-            { v: '110101', n: '东城区' },
-            { v: '110102', n: '西城区' },
-            { v: '110105', n: '朝阳区' },
-            { v: '110106', n: '丰台区' },
-            { v: '110107', n: '石景山区' },
-            { v: '110108', n: '海淀区' },
-            { v: '110109', n: '门头沟区' },
-            { v: '110111', n: '房山区' },
-            { v: '110112', n: '通州区' },
-            { v: '110113', n: '顺义区' },
-            { v: '110114', n: '昌平区' },
-            { v: '110115', n: '大兴区' },
-            { v: '110116', n: '怀柔区' },
-            { v: '110117', n: '平谷区' },
-            { v: '110118', n: '密云区' },
-            { v: '110119', n: '延庆区' }
-          ]
-        }
-      ]
+  divisionUtil = new DivisionUtil(sourceData);
+  divisionUtil1 = new DivisionUtil<DivisionData, CascaderOption>(sourceData, {
+    output: {
+      code: 'value',
+      name: 'label',
+      children: 'children'
     }
-  ]);
+  });
 });
 
-describe('DivisionUtil', () => {
+describe('DivisionUtil default config', () => {
   describe('getChildrenByCode', () => {
     it('获取省级数据', () => {
       expect(divisionUtil.getChildrenByCode()).toEqual([{ v: '110000', n: '北京市' }]);
@@ -69,7 +81,7 @@ describe('DivisionUtil', () => {
     });
   })
 
-  describe('getChildrenByCode', () => {
+  describe('getNameByCode', () => {
     it('获取省级名称', () => {
       expect(divisionUtil.getNameByCode('110000')).toEqual('北京市');
     });
@@ -87,3 +99,56 @@ describe('DivisionUtil', () => {
     });
   })
 });
+
+describe('DivisionUtil other config', () => {
+  describe('getChildrenByCode', () => {
+    it('获取省级数据', () => {
+      expect(divisionUtil1.getChildrenByCode()).toEqual([{ value: '110000', label: '北京市' }]);
+    });
+
+    it('获取市级数据', () => {
+      expect(divisionUtil1.getChildrenByCode('110000')).toEqual([
+        { value: '110100', label: '市辖区' }
+      ]);
+    });
+
+    it('获取区域数据', () => {
+      expect(divisionUtil1.getChildrenByCode('110100')).toEqual([
+        { value: '110101', label: '东城区' },
+        { value: '110102', label: '西城区' },
+        { value: '110105', label: '朝阳区' },
+        { value: '110106', label: '丰台区' },
+        { value: '110107', label: '石景山区' },
+        { value: '110108', label: '海淀区' },
+        { value: '110109', label: '门头沟区' },
+        { value: '110111', label: '房山区' },
+        { value: '110112', label: '通州区' },
+        { value: '110113', label: '顺义区' },
+        { value: '110114', label: '昌平区' },
+        { value: '110115', label: '大兴区' },
+        { value: '110116', label: '怀柔区' },
+        { value: '110117', label: '平谷区' },
+        { value: '110118', label: '密云区' },
+        { value: '110119', label: '延庆区' }
+      ]);
+    });
+  });
+
+  describe('getNameByCode', () => {
+    it('获取省级名称', () => {
+      expect(divisionUtil1.getNameByCode('110000')).toEqual('北京市');
+    });
+
+    it('获取市级名称', () => {
+      expect(divisionUtil1.getNameByCode('110100')).toEqual('市辖区');
+    });
+
+    it('获取区级名称', () => {
+      expect(divisionUtil1.getNameByCode('110112')).toEqual('通州区');
+    });
+
+    it('获取不存在的省市区', () => {
+      expect(divisionUtil1.getNameByCode('999999')).toEqual('');
+    });
+  })
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,55 @@
 import { DivisionData } from './types';
 
-class DivisionUtil {
-  private divisions: DivisionData[];
-  private cache: Record<string, any>;
+type SourceKeys = 'code' | 'name' | 'children';
 
-  constructor(list: DivisionData[]) {
+type Options = Record<SourceKeys, string>;
+
+const defaultOptions: Options = {
+  code: 'v',
+  name: 'n',
+  children: 'c'
+}
+
+class DivisionUtil<S = DivisionData, O = DivisionData> {
+  private divisions: S[];
+  private cache: Record<string, any>;
+  private sourceOptions: Options;
+  private outputOptions: Options;
+
+  constructor(
+    list: S[],
+    options?: {
+      source?: Partial<Options>,
+      output?: Partial<Options>
+    }
+  ) {
     this.divisions = list;
     this.cache = {};
+    this.sourceOptions = {...defaultOptions, ...options?.source};
+    this.outputOptions = {...defaultOptions, ...options?.output};
   }
 
   /**
    * 获取数据源
    */
-  getSourceData = () => {
+  getSourceData = (): S[] => {
     return this.divisions ?? [];
   }
 
   /**
    * 获取省级别的数据
    */
-  getProvinces = (): DivisionData[] => {
+  getProvinces = (): O[] => {
     if (!Array.isArray(this.divisions) || this.divisions.length === 0) return [];
 
     if (this.cache.divisions) {
       return this.cache.divisions;
     } else {
-      const divisions = this.divisions.map((item) => ({ v: item.v, n: item.n }));
+      const divisions = this.divisions.map(this._formatDivision);
 
       this.cache.divisions = divisions;
 
-      return divisions;
+      return divisions as O[];
     }
   };
 
@@ -44,15 +64,15 @@ class DivisionUtil {
     return codes;
   }
 
-  _getProvinceData = (code: string): DivisionData => {
-    let province: DivisionData;
+  _getProvinceData = (code: string): S => {
+    let province: S;
 
     // 尝试读取缓存
     if (this.cache[code]) {
       province = this.cache[code];
     } else {
       for (let i = 0; i < this.divisions.length; i++) {
-        if (this.divisions[i].v.startsWith(code)) {
+        if (this.divisions[i][`${this.sourceOptions.code}`].startsWith(code)) {
           this.cache[code] = province = this.divisions[i];
           break;
         }
@@ -62,11 +82,18 @@ class DivisionUtil {
     return province;
   }
 
+  _formatDivision = (item) => {
+    return {
+      [`${this.outputOptions.code}`]: item[this.sourceOptions.code],
+      [`${this.outputOptions.name}`]: item[this.sourceOptions.name]
+    }
+  }
+
   /**
    * 获取指定省市区编码的下一级数据。若不指定，则获取省一级数据
    * @param code 省市区编码
    */
-  getChildrenByCode = (code?: string) => {
+  getChildrenByCode = (code?: string): O[] => {
     if (!code) return this.getProvinces();
     if (typeof code !== 'string' || code.length !== 6) return [];
     if (this.cache[code]) return this.cache[code];
@@ -76,19 +103,19 @@ class DivisionUtil {
 
     if (!province) return [];
 
-    let children = province.c ?? [];
+    let children = province[`${this.sourceOptions.children}`] ?? [];
 
     for (let i = 1; i < codes.length; i++) {
       const currentCode = codes.slice(0, i).reduce((next, current) => next + current, '');
 
       for (let item of children) {
-        if (item.v.startsWith(currentCode)) {
-          children = item.c ?? [];
+        if (item[`${this.sourceOptions.code}`].startsWith(currentCode)) {
+          children = item[`${this.sourceOptions.children}`] ?? [];
         }
       }
     }
 
-    const list = children.map(item => ({ v: item.v, n: item.n }));
+    const list = children.map(this._formatDivision);
 
     this.cache[code] = list;
 
@@ -110,14 +137,14 @@ class DivisionUtil {
     for (let i = 1; i < codes.length; i++) {
       const currentCode = codes.slice(0, i + 1).reduce((next, current) => next + current, '');
 
-      for (let item of (divisionData.c || [])) {
-        if (item.v.startsWith(currentCode)) {
+      for (let item of (divisionData[`${this.sourceOptions.children}`] || [])) {
+        if (item[`${this.sourceOptions.code}`].startsWith(currentCode)) {
           divisionData = item;
         }
       }
     }
 
-    return divisionData.n ?? '';
+    return divisionData[`${this.sourceOptions.name}`] ?? '';
   }
 }
 
